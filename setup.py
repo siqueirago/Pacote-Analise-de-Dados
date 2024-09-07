993237da03b7d7f36cda94d3f7aed555955e5fd7
Julio <siqueiragomes123@gmail.com>
2024-09-07 19:01:33 -0300

    Alterações

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="Pacote Analise Dados",  # Nome do pacote
-    version="0.1",
+    name="analise_de_dados",  # Nome do pacote
+    version="0.0.1",
     packages=find_packages(where="analise_de_dados"),
     package_dir={"": "analise_de_dados"},
     description="Um pacote de exemplo para análise de dados.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     author="Julio",
-    author_email="seu.email@example.com",
+    author_email="siqueiragomes123>gmail.com",
     url="https://github.com/siqueirago/pacote-analise-dados",  # URL do projeto
     classifiers=[
         "Programming Language :: Python :: 3",
